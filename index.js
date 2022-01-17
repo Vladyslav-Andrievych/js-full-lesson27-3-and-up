31803f4bf718b753249cc5cc2adba862e7bcef7e
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-01-17 18:27:46 +0200

    lesson 33 1 add check to location

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const renderUserData = (userData) => {
 
   userAvatarElem.src = avatar_url;
   userNameElem.textContent = name;
-  userLocationElem.textContent = location;
+  userLocationElem.textContent = location ? `from ${location}` : '';
 };
 
 const onSearchClick = () => {
